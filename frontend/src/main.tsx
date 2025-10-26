2abf8c0985da800333d0b19eb1b805ce3e28eb06
GabrielLiuti <gabrieliuti01@gmail.com>
2025-10-26 16:17:06 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; // Se você tiver um CSS global
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/fiscalmanager-total">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
