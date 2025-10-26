eb739bda6002b29e91f3e5c0f07215ddf5cd4ce8
GabrielLiuti <gabrieliuti01@gmail.com>
2025-10-25 22:50:45 -0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,6 @@ import helmet from "helmet";
 import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
-import * as authRoutes from "./modules/auth/auth.router";
-import * as empresasRoutes from "./modules/empresas/empresas.router";
-import * as produtosRoutes from "./modules/produtos/produtos.router";
-import * as notasRoutes from "./modules/notas/notas.router";
-
 
 dotenv.config();
 
@@ -18,19 +13,19 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-// Swagger setup
+// Swagger
 const swaggerDocument = YAML.load("./swagger.yaml");
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Rotas principais
-import authRoutes from "./modules/auth/auth.router";
-import empresasRoutes from "./modules/empresas/empresas.router";
-import produtosRoutes from "./modules/produtos/produtos.router";
-import notasRoutes from "./modules/notas/notas.router";
+import authRouter from "./modules/auth/auth.router";
+import empresasRouter from "./modules/empresas/empresas.router";
+import produtosRouter from "./modules/produtos/produtos.router";
+import notasRouter from "./modules/notas/notas.router";
 
-app.use("/api/auth", authRoutes);
-app.use("/api/empresas", empresasRoutes);
-app.use("/api/produtos", produtosRoutes);
-app.use("/api/notas", notasRoutes);
+app.use("/api/auth", authRouter);
+app.use("/api/empresas", empresasRouter);
+app.use("/api/produtos", produtosRouter);
+app.use("/api/notas", notasRouter);
 
 export default app;
